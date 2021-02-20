7f292824e25bdf680e5dc75efa8a6007c5ec09ad
duvu <hoaivubk@gmail.com>
2021-02-20 23:11:13 +0700

    RL1#2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Press the green button in the gutter to run the script.
-from reinforcement_learning import CustomEnv
+from reinforcement_learning_environment import CustomEnv
 
 if __name__ == '__main__':
     df = pd.read_csv('./pricedata.csv')
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     train_df = df[:-720 - lookback_window_size]
     test_df = df[-720 - lookback_window_size:]  # 30days
 
-    train_env = CustomEnv(train_df, lookback_window_size=lookback_window_size)
+    train_env = CustomEnv(train_df, lookback_window_size=lookback_window_size, render_range=100)
     test_env = CustomEnv(test_df, lookback_window_size=lookback_window_size)
 
-    CustomEnv.Random_games(train_env, train_episodes=10, train_batch_size=500)
+    CustomEnv.random_games(train_env, train_episodes=1, train_batch_size=100)
---
+++ utils.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import os
+
+
+def write_to_file(Date, net_worth, filename='{}.txt'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))):
+    for i in net_worth:
+        Date += " {}".format(i)
+        # print(Date)
+        if not os.path.exists('logs'):
+            os.makedirs('logs')
+        file = open("logs/" + filename, 'a+')
+        file.write(Date + "\n")
+        file.close()
